cdbdb9fa3c16d3716a340e87533c597b38087b0d
MarceloRodrigoNeves <marcelo.m7.sistemas@gmail.com>
2023-05-12 16:23:39 -0300

    feat: Incluido parte do reteiro iniciando a criar e iniciar projeto angular.

--- roteiroCursoMapperidea.mm
+++ roteiroCursoMapperidea.mm
@@ -12,7 +12,7 @@
 <node CREATED="1683892445388" MODIFIED="1683892465787" TEXT="requisitos de sistemas para proseguir o curso"/>
 </node>
 </node>
-<node CREATED="1683892251953" MODIFIED="1683894482049" POSITION="right" TEXT="Backend com json server">
+<node CREATED="1683892251953" ID="ID_1447798704" MODIFIED="1683894482049" POSITION="right" TEXT="Backend com json server">
 <icon BUILTIN="folder"/>
 <node CREATED="1683892510940" MODIFIED="1683892900007" TEXT="Criar pasta:">
 <icon BUILTIN="full-1"/>
@@ -48,12 +48,12 @@
 <node CREATED="1683896108360" MODIFIED="1683896126069" TEXT="package.json">
 <icon BUILTIN="list"/>
 <node CREATED="1683896129420" MODIFIED="1683896144324" TEXT="Na parte de &quot;scripts&quot;">
-<node CREATED="1683896144324" MODIFIED="1683896174766" TEXT="apagar o conteudo:">
+<node CREATED="1683896144324" ID="ID_1365965601" MODIFIED="1683896174766" TEXT="apagar o conteudo:">
 <node CREATED="1683896175160" MODIFIED="1683898205290" TEXT="&quot;test&quot;: &quot;echo \&quot;Error: no test specified\&quot; &amp;&amp; exit 1&quot;">
 <icon BUILTIN="tag_green"/>
 </node>
 </node>
-<node CREATED="1683896434586" MODIFIED="1683896440597" TEXT="Incluir o conteudo:">
+<node CREATED="1683896434586" ID="ID_1605685747" MODIFIED="1683896440597" TEXT="Incluir o conteudo:">
 <node CREATED="1683896441290" MODIFIED="1683898205290" TEXT="&quot;start&quot;: &quot;json-server --watch db.json --port 3001&quot;">
 <icon BUILTIN="tag_green"/>
 </node>
@@ -61,14 +61,14 @@
 </node>
 </node>
 </node>
-<node CREATED="1683896662145" MODIFIED="1683896820304" TEXT="Executa o comando:">
+<node CREATED="1683896662145" ID="ID_733260442" MODIFIED="1683896820304" TEXT="Executa o comando:">
 <icon BUILTIN="desktop_new"/>
-<node CREATED="1683897073891" MODIFIED="1683897079238" TEXT="npm start"/>
+<node CREATED="1683897073891" ID="ID_774129317" MODIFIED="1683897079238" TEXT="npm start"/>
 </node>
 </node>
 <node CREATED="1683897497393" ID="ID_321503316" MODIFIED="1683897514388" TEXT="Abrir o postman">
 <icon BUILTIN="full-5"/>
-<node CREATED="1683899449671" ID="ID_157335987" MODIFIED="1683899594745" TEXT="Cria requisi&#xe7;&#xf5;es">
+<node CREATED="1683899449671" FOLDED="true" ID="ID_157335987" MODIFIED="1683899594745" TEXT="Cria requisi&#xe7;&#xf5;es">
 <node CREATED="1683899455487" ID="ID_316336714" MODIFIED="1683899464612" TEXT="Listar products">
 <node CREATED="1683897595934" ID="ID_569960746" MODIFIED="1683899471199" TEXT="Tipo:">
 <node CREATED="1683897598757" MODIFIED="1683897601709" TEXT="GET"/>
@@ -196,8 +196,72 @@
 </node>
 </node>
 </node>
-<node CREATED="1683892256832" MODIFIED="1683894490368" POSITION="right" TEXT="Frontend">
+<node CREATED="1683914773304" ID="ID_1610956331" MODIFIED="1683914788636" POSITION="right" TEXT="Instalar Angular CLI">
+<icon BUILTIN="down"/>
+<node CREATED="1683896662145" ID="ID_722147802" MODIFIED="1683896820304" TEXT="Executa o comando:">
+<icon BUILTIN="desktop_new"/>
+<node CREATED="1683914812104" ID="ID_1901725310" MODIFIED="1683915009875" TEXT="npm i @angular/cli@9.1.15"/>
+<node CREATED="1683915745816" ID="ID_527707911" MODIFIED="1683915749932" TEXT="ng version"/>
+</node>
+</node>
+<node CREATED="1683892256832" ID="ID_637378880" MODIFIED="1683894490368" POSITION="right" TEXT="Frontend">
 <icon BUILTIN="Descriptor.window.iframe"/>
+<node CREATED="1683915658067" ID="ID_1573966263" MODIFIED="1683915670851" TEXT="Criar o projeto angular">
+<icon BUILTIN="full-1"/>
+<node CREATED="1683896662145" ID="ID_1227375264" MODIFIED="1683896820304" TEXT="Executa o comando:">
+<icon BUILTIN="desktop_new"/>
+<node CREATED="1683915673788" ID="ID_1080140764" MODIFIED="1683915800754" TEXT="ng new frontend --minimal"/>
+</node>
+<node CREATED="1683915833137" ID="ID_1372755729" MODIFIED="1683915868172" TEXT="Responder as perguntas:">
+<icon BUILTIN="edit"/>
+<node CREATED="1683915839993" ID="ID_1798264056" MODIFIED="1683915841456" TEXT="Would you like to add Angular routing? (y/N)">
+<node CREATED="1683915841456" ID="ID_576164369" MODIFIED="1683915845913" TEXT="Resposta:">
+<node CREATED="1683915845914" ID="ID_1505490558" MODIFIED="1683915910218" TEXT="Y">
+<icon BUILTIN="tag_green"/>
+</node>
+</node>
+</node>
+<node CREATED="1683915865060" ID="ID_725284279" MODIFIED="1683915865060" TEXT="Which stylesheet format would you like to use? (Use arrow keys)">
+<node CREATED="1683915841456" ID="ID_125458922" MODIFIED="1683915845913" TEXT="Resposta:">
+<node CREATED="1683915899336" ID="ID_780948303" MODIFIED="1683915910217" TEXT="CSS">
+<icon BUILTIN="tag_green"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1683917020418" ID="ID_671833175" MODIFIED="1683917039257" TEXT="Inicar parte frontend">
+<icon BUILTIN="full-2"/>
+<node CREATED="1683896662145" ID="ID_1152436455" MODIFIED="1683896820304" TEXT="Executa o comando:">
+<icon BUILTIN="desktop_new"/>
+<node CREATED="1683897073891" ID="ID_1958011860" MODIFIED="1683897079238" TEXT="npm start"/>
+</node>
+<node CREATED="1683917299005" ID="ID_1805885170" MODIFIED="1683917335899" TEXT="Abrir navegador no endere&#xe7;o:">
+<icon BUILTIN="Descriptor.window.step"/>
+<node CREATED="1683917314080" ID="ID_186726484" MODIFIED="1683917316569" TEXT="http://localhost:4200/"/>
+</node>
+<node CREATED="1683918427809" ID="ID_133617613" MODIFIED="1683919312595" TEXT="Alterar o arquivo:">
+<icon BUILTIN="edit"/>
+<node CREATED="1683918493649" ID="ID_1946773219" MODIFIED="1683919354391" TEXT="frontend/angular.json">
+<icon BUILTIN="list"/>
+<node CREATED="1683918533469" ID="ID_293602563" MODIFIED="1683918541878" TEXT="Na parte de &quot;@schematics/angular:component&quot;">
+<node CREATED="1683896144324" ID="ID_155830693" MODIFIED="1683896174766" TEXT="apagar o conteudo:">
+<node CREATED="1683896175160" ID="ID_778959458" MODIFIED="1683918613816" TEXT="          &quot;inlineTemplate&quot;: true,&#xa;          &quot;inlineStyle&quot;: true,">
+<icon BUILTIN="tag_green"/>
+</node>
+</node>
+<node CREATED="1683896434586" ID="ID_1072488174" MODIFIED="1683896440597" TEXT="Incluir o conteudo:">
+<node CREATED="1683896175160" ID="ID_444505900" MODIFIED="1683918626677" TEXT="          &quot;inlineTemplate&quot;: false,&#xa;          &quot;inlineStyle&quot;: false,">
+<icon BUILTIN="tag_green"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1683919251160" ID="ID_1827413991" MODIFIED="1683919270874" TEXT="Alterar projeto frontend">
+<icon BUILTIN="full-3"/>
+</node>
 </node>
 </node>
 </map>
