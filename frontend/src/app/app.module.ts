8025ba15f79acc96e29760ff67817833d3199f0e
MarceloRodrigoNeves <marcelo.m7.sistemas@gmail.com>
2023-05-18 12:15:23 -0300

    fix: Ajustado posicoes das funcoes e imports.

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -23,14 +23,13 @@ import { HeaderComponent } from './components/template/header/header.component';
 import { FooterComponent } from './components/template/footer/footer.component';
 import { NavComponent } from './components/template/nav/nav.component';
 import { HomeComponent } from './views/home/home.component';
+registerLocaleData(localePt);
 
 import { ProductCrudComponent } from './views/product-crud/product-crud.component';
 import { ProductCreateComponent } from './components/product/product-create/product-create.component';
 import { ProductReadComponent } from './components/product/product-read/product-read.component';
 import { ProductUpdateComponent } from './components/product/product-update/product-update.component';
 import { ProductDeleteComponent } from './components/product/product-delete/product-delete.component';
-
-registerLocaleData(localePt);
 
 
 @NgModule({
--- frontend/src/app/components/product/product.service.ts
+++ frontend/src/app/components/product/product.service.ts
@@ -1,25 +1,17 @@
 import { Injectable } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { HttpClient } from '@angular/common/http';
-import { Product } from './product.model';
 import { Observable, EMPTY } from 'rxjs';
+import { Product } from './product.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
 
-  baseUrl = "http://localhost:3001/products";
-
   constructor(private snackBar: MatSnackBar, private http: HttpClient) { }
 
-  showMessage(msg: string): void {
-    this.snackBar.open(msg, 'x', {
-      duration: 3000,
-      horizontalPosition: "right",
-      verticalPosition: "top"
-    })
-  }
+  baseUrl = "http://localhost:3001/products";
 
   create(product: Product): Observable<Product> {
     return this.http.post<Product>(this.baseUrl, product);
@@ -39,8 +31,17 @@ export class ProductService {
     return this.http.put<Product>(url, product);
   }
 
-  delete(id: string): Observable<Product>{
+  delete(id: string): Observable<Product> {
     const url = this.baseUrl + "/" + id;
     return this.http.delete<Product>(url);
   }
+
+  showMessage(msg: string): void {
+    this.snackBar.open(msg, 'x', {
+      duration: 3000,
+      horizontalPosition: "right",
+      verticalPosition: "top"
+    })
+  }
+
 }
