e742ef5b13350119cba0fd6bcd1ea62f66f48acc
MarceloRodrigoNeves <marcelo.m7.sistemas@gmail.com>
2023-05-19 15:15:51 -0300

    fix: Ajustado uma quebra de linha, para organizar melhor o codigo.

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { HomeComponent } from './views/home/home.component';
+
 import { ProductCrudComponent } from './views/product-crud/product-crud.component';
 import { ProductCreateComponent } from './components/product/product-create/product-create.component';
 import { ProductUpdateComponent } from './components/product/product-update/product-update.component';
