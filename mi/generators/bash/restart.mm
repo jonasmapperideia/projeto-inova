46927de395b04222803eca9acf23bdeb1977eec5
Marcelo Neves <marcelo@neoinix.com.br>
2023-05-29 12:21:19 -0300

    feat: Includo commandos para excluir arquivos frontend e gerar novamente.

--- mi/generators/bash/restart.mm
+++ mi/generators/bash/restart.mm
@@ -7,7 +7,7 @@
 <node CREATED="1685109976879" MODIFIED="1685127541421" TEXT="mi g angularCRUD bash restart projectName=angularCRUD"/>
 <node CREATED="1685109978872" MODIFIED="1685127755305" TEXT="mi g angularCRUD bash restart projectName=angularCRUD &gt; restart.sh"/>
 </node>
-<node CREATED="1684159426363" FOLDED="true" MODIFIED="1685108986133" POSITION="right" TEXT="parameters">
+<node CREATED="1684159426363" FOLDED="true" ID="ID_622174751" MODIFIED="1685108986133" POSITION="right" TEXT="parameters">
 <icon BUILTIN="element"/>
 <node CREATED="1684159426364" FOLDED="true" MODIFIED="1685109028034" TEXT="projectName">
 <icon BUILTIN="element"/>
@@ -16,7 +16,7 @@
 </node>
 </node>
 </node>
-<node CREATED="1684161691515" FOLDED="true" MODIFIED="1685108855186" POSITION="right" TEXT="patterns">
+<node CREATED="1684161691515" ID="ID_1218710530" MODIFIED="1685108855186" POSITION="right" TEXT="patterns">
 <icon BUILTIN="element"/>
 <node CREATED="1685109369407" FOLDED="true" MODIFIED="1685109501597" TEXT="init">
 <icon BUILTIN="Descriptor.grouping"/>
@@ -33,7 +33,7 @@
 </node>
 </node>
 </node>
-<node CREATED="1685126815502" FOLDED="true" MODIFIED="1685126816938" TEXT="Starting Mapperidea project">
+<node CREATED="1685126815502" FOLDED="true" ID="ID_1121042032" MODIFIED="1685126816938" TEXT="Starting Mapperidea project">
 <icon BUILTIN="Descriptor.grouping"/>
 <node CREATED="1685126913005" FOLDED="true" MODIFIED="1685126914407" TEXT="startingMapperideaProject">
 <icon BUILTIN="element"/>
@@ -45,7 +45,7 @@
 </node>
 </node>
 </node>
-<node CREATED="1685127000272" FOLDED="true" MODIFIED="1685127004566" TEXT="Generate backend file">
+<node CREATED="1685127000272" FOLDED="true" ID="ID_650119466" MODIFIED="1685127004566" TEXT="Generate backend file">
 <icon BUILTIN="Descriptor.grouping"/>
 <node CREATED="1685127006308" FOLDED="true" MODIFIED="1685127024933" TEXT="generateBackendFile">
 <icon BUILTIN="element"/>
@@ -57,25 +57,25 @@
 </node>
 </node>
 </node>
-<node CREATED="1685127000272" FOLDED="true" MODIFIED="1685127157843" TEXT="Generate frontend file">
+<node CREATED="1685127000272" ID="ID_989117971" MODIFIED="1685127157843" TEXT="Generate frontend file">
 <icon BUILTIN="Descriptor.grouping"/>
-<node CREATED="1685127274614" FOLDED="true" MODIFIED="1685127276160" TEXT="generateFrontendFile">
+<node CREATED="1685127274614" ID="ID_1876386791" MODIFIED="1685127276160" TEXT="generateFrontendFile">
 <icon BUILTIN="element"/>
-<node CREATED="1685127463158" MODIFIED="1685127879409" TEXT="# Gera arquivo frontend ------------------------------------------------------&#xa;echo &quot; --- Gerando arquivos do frontend do sistema na linguagem angular9 --- &quot;&#xa;cd ../frontend&#xa;&#xa;mapperidea generate {{ $projectName }} angular9 navComponentHtml &gt; src/app/components/template/nav/nav.component.html&#xa;mapperidea generate {{ $projectName }} angular9 appModuleTs &gt; src/app/app.module.ts&#xa;mapperidea generate {{ $projectName }} angular9 appRoutingModuleTs &gt; src/app/app-routing.module.ts&#xa;# ----------------------------------------------------------------------------">
+<node CREATED="1685127463158" ID="ID_247821300" MODIFIED="1685371083225" TEXT="# Gera arquivo frontend ------------------------------------------------------&#xa;echo &quot; --- Gerando arquivos do frontend do sistema na linguagem angular9 --- &quot;&#xa;cd ../frontend&#xa;&#xa;mapperidea generate {{ $projectName }} angular9 navComponentHtml &gt; src/app/components/template/nav/nav.component.html&#xa;mapperidea generate {{ $projectName }} angular9 appModuleTs &gt; src/app/app.module.ts&#xa;mapperidea generate {{ $projectName }} angular9 appRoutingModuleTs &gt; src/app/app-routing.module.ts&#xa;&#xa;cd ../bash&#xa;rm ./angular9/*.sh&#xa;rm -rf ../frontend/src/app/windows&#xa;mapperidea generate {{ $projectName }} bash generateBashScreens projectName=angularCRUD &gt; angular9/generateBashScreens.sh&#xa;mapperidea generate {{ $projectName }} bash executeBashScreens &gt; angular9/executeBashScreens.sh&#xa;&#xa;cd ./angular9&#xa;bash ./generateBashScreens.sh&#xa;bash ./executeBashScreens.sh&#xa;# ----------------------------------------------------------------------------">
 <icon BUILTIN="tag_green"/>
 </node>
 </node>
 </node>
 </node>
-<node CREATED="1684160926435" FOLDED="true" MODIFIED="1684160965104" POSITION="right" TEXT="start">
+<node CREATED="1684160926435" ID="ID_765259783" MODIFIED="1684160965104" POSITION="right" TEXT="start">
 <icon BUILTIN="element"/>
-<node CREATED="1684160926436" FOLDED="true" MODIFIED="1685107011059" TEXT="match">
+<node CREATED="1684160926436" FOLDED="true" ID="ID_1849580085" MODIFIED="1685107011059" TEXT="match">
 <icon BUILTIN="element"/>
 <node CREATED="1684160926437" MODIFIED="1685126545038" TEXT="/classes/class[1]">
 <icon BUILTIN="tag_green"/>
 </node>
 </node>
-<node CREATED="1684160926438" FOLDED="true" MODIFIED="1685107011061" TEXT="body">
+<node CREATED="1684160926438" ID="ID_655318274" MODIFIED="1685107011061" TEXT="body">
 <icon BUILTIN="element"/>
 <node CREATED="1685109369407" FOLDED="true" MODIFIED="1685109372472" TEXT="init">
 <icon BUILTIN="Descriptor.grouping"/>
@@ -86,7 +86,7 @@
 </node>
 </node>
 </node>
-<node CREATED="1685126815502" FOLDED="true" MODIFIED="1685126816938" TEXT="Starting Mapperidea project">
+<node CREATED="1685126815502" FOLDED="true" ID="ID_874370537" MODIFIED="1685126816938" TEXT="Starting Mapperidea project">
 <icon BUILTIN="Descriptor.grouping"/>
 <node CREATED="1671891592531" FOLDED="true" MODIFIED="1685109084086" TEXT="write-pattern">
 <icon BUILTIN="element"/>
@@ -95,7 +95,7 @@
 </node>
 </node>
 </node>
-<node CREATED="1685127000272" FOLDED="true" MODIFIED="1685127004566" TEXT="Generate backend file">
+<node CREATED="1685127000272" FOLDED="true" ID="ID_377959736" MODIFIED="1685127004566" TEXT="Generate backend file">
 <icon BUILTIN="Descriptor.grouping"/>
 <node CREATED="1671891592531" FOLDED="true" MODIFIED="1685109084086" TEXT="write-pattern">
 <icon BUILTIN="element"/>
@@ -104,9 +104,9 @@
 </node>
 </node>
 </node>
-<node CREATED="1685127000272" FOLDED="true" MODIFIED="1685127157843" TEXT="Generate frontend file">
+<node CREATED="1685127000272" ID="ID_1867106553" MODIFIED="1685127157843" TEXT="Generate frontend file">
 <icon BUILTIN="Descriptor.grouping"/>
-<node CREATED="1671891592531" FOLDED="true" MODIFIED="1685109084086" TEXT="write-pattern">
+<node CREATED="1671891592531" ID="ID_112090222" MODIFIED="1685109084086" TEXT="write-pattern">
 <icon BUILTIN="element"/>
 <node CREATED="1685127237841" MODIFIED="1685127248891" TEXT="generateFrontendFile">
 <icon BUILTIN="tag_green"/>
